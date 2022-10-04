78f6ad12907bb64db13698de9b32cbbdc7ede9f7
cesaraltuve <cesar_altuve@hotmail.com>
2022-10-04 09:15:00 -0300

    first commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2022 <a href="https://www.linkedin.com/in/cesar-altuve-a1a244a3/" target="_blank">Realizado por cesaraltuve@gmail.com</a></strong>
+
+	
+
+
+</footer>
